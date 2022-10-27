0e9fbd388d2985ec2a45199907720d33e9c6f399
Bangtx <bangtran.hha@gmail.com>
2022-10-27 09:34:40 +0700

    123

--- db.sql
+++ db.sql
@@ -8,13 +8,25 @@ create table sensor (
 );
 
 
+create table casting_machine (
+	id bigint NOT NULL IDENTITY PRIMARY KEY,
+	name text,
+	datetime datetime default CURRENT_TIMESTAMP
+);
+
+
 create table temperature (
 	id bigint NOT NULL IDENTITY PRIMARY KEY,
 	temp int,
 	sensor_id bigint FOREIGN KEY REFERENCES sensor(id),
 	status int,
+	casting_machine_id bigint FOREIGN KEY REFERENCES casting_machine(id),
 	datetime datetime default CURRENT_TIMESTAMP
 );
+
+
+insert into casting_machine (name) values ('may 1')
+
 
 insert into sensor (name, host, port ,unit)
 values ('sensor 1', '192.168.1.254', 8880, 1),
